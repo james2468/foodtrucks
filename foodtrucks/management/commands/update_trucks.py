66099f6bdd8db2e09a42053ee4c45b4cd17faa28
James Turk <james.p.turk@gmail.com>
2010-06-14 15:22:45 -0400

    add geocode method

--- foodtrucks/management/commands/update_trucks.py
+++ foodtrucks/management/commands/update_trucks.py
@@ -1,11 +1,17 @@
 from django.core.management.base import NoArgsCommand
 
 from foodtrucks.models import Restaurant, Update
+from foodtrucks.utils import geocode
+
+fakes = ['12th and I NW, Washington DC', '19th and N NW, Washington DC',
+         'Dupont Circle, Washington DC', "L'Enfant Plaza, Washington DC",
+         '22nd and H NW, Washington DC']
+import random
 
 class Command(NoArgsCommand):
 
     def handle_noargs(self, **options):
 
-        
-
+        for r in Restaurant.objects.all():
+            Update.objects.create(restaurant=r, location=geocode(random.choice(fakes)))
 
---
+++ foodtrucks/utils.py
@@ -0,0 +1,32 @@
+from django.conf import settings
+from django.utils import simplejson
+from django.contrib.gis.geos import Point
+
+import urllib
+
+def geocode(location):
+    """
+        Given a location return a ``Point`` pair.
+    """
+    params = {
+     'q': location,
+     'key': settings.GMAPS_KEY,
+     'sensor': 'false',
+     'output': 'json',
+    }
+    url = 'http://maps.google.com/maps/geo?%s' % urllib.urlencode(params)
+
+    try:
+        resp = urllib.urlopen(url).read()
+        resp = simplejson.loads(resp)
+        if resp['Status']['code'] == 200:
+            pmark = resp['Placemark'][0]
+            lon,lat = pmark['Point']['coordinates'][0:2]
+            return Point(lat, lon)
+        else:
+            print resp
+            return None
+    except Exception, e:
+        print e
+        return None
+
