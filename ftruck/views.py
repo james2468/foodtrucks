2088bb68aa3bcdac0fa5ef9891f4353bd859bd16
James Turk <james.p.turk@gmail.com>
2010-06-15 11:31:07 -0400

    info windows now show up

--- ftruck/views.py
+++ ftruck/views.py
@@ -4,7 +4,13 @@ from ftruck.models import Restaurant
 def mainmap(request):
     """ render a map with the current location of trucks """
 
-    restaurants = Restaurant.objects.all()
+    retval = []
+    for r in Restaurant.objects.all():
+        update = r.latest_update()
+        retval.append({'name': r.name,
+                       'avatar': r.avatar,
+                       'update': update.update,
+                       'website': r.website,
+                       'location': update.location})
 
-    return render_to_response('mainmap.html',
-                              {'restaurants':restaurants})
+    return render_to_response('mainmap.html', {'trucks':retval})
