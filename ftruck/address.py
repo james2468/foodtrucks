c680fd56978824fda8193ec00e5f9ca0aac41479
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-06-15 16:44:41 -0400

    minor edits to extraction

--- ftruck/address.py
+++ ftruck/address.py
@@ -3,10 +3,10 @@
 import re
 import sys
 
-whitelist = "(dupont|u st|farragut|gallery pl)"
+whitelist = "(dupont|u st|farragut|gallery pl|union station|L'Enfant|McPherson)"
 numbered = "(1st|2nd|3rd|[0-9]+(th)?)"
 street = "(\w+( ave| st)?|%(numbered)s)" % {'numbered': numbered}
-cross_street = "(%(numbered)s ?((and)|&) ?%(street)s)" % {'street': street, 'numbered': numbered}
+cross_street = "((%(numbered)s ?((and)|&) ?%(street)s)|(%(street)s ?((and)|&) ?%(numbered)s))" % {'street': street, 'numbered': numbered}
 exact = "([0-9]+ \w+ (ave|st))"
 address = '(((%(street)s between )?%(cross)s)|%(exact)s|%(whitelist)s)' % \
     {'street': street, 'cross': cross_street, 'exact': exact, 'whitelist': whitelist}
