2ea41dd0f338d271147a831c491be2bf9546fd13
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-06-15 11:59:08 -0400

    Began work on pulling back real tweets. Not correctly writing to DB yet.

--- ftruck/management/commands/update_trucks.py
+++ ftruck/management/commands/update_trucks.py
@@ -1,17 +1,48 @@
 from django.core.management.base import NoArgsCommand
+from django.db.models import Max
 
 from ftruck.models import Restaurant, Update
 from ftruck.utils import geocode
 
-fakes = ['12th and I NW, Washington DC', '19th and N NW, Washington DC',
-         'Dupont Circle, Washington DC', "L'Enfant Plaza, Washington DC",
-         '22nd and H NW, Washington DC']
-import random
+# various things stolen from Mike Verdone's excellent Twitter wrapper <http://mike.verdone.ca/twitter/>
+
+import urllib2
+
+def _py26OrGreater():
+    import sys
+    return sys.hexversion > 0x20600f0
+
+if _py26OrGreater():
+    import json
+else:
+    import simplejson as json
+
+def get_new_statuses(user='sbma44', list_name='dc-food-carts', since_id=None):
+    if since_id is not None:
+        since_id = "&since_id=%d" % since_id
+        
+    url = 'http://api.twitter.com/1/%s/lists/%s/statuses.json?per_page=200%s' % (user, list_name, since_id)
+    req = urllib2.Request(url)
+
+    try:
+        handle = urllib2.urlopen(req)
+        return json.loads(handle.read())
+    except urllib2.HTTPError, e:
+        if (e.code == 304):
+            return []
+        else:
+            raise e
+
+
+
 
 class Command(NoArgsCommand):
 
     def handle_noargs(self, **options):
-
-        for r in Restaurant.objects.all():
-            Update.objects.create(restaurant=r, location=geocode(random.choice(fakes)), twitter_status_id=4)
-
+        
+        since = Update.objects.aggregate(Max('twitter_status_id'))['twitter_status_id__max']
+        
+        for status in get_new_statuses(since_id=since):
+            update = Update.from_json(status)
+            update.save()
+       
--- ftruck/models.py
+++ ftruck/models.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django.contrib.gis.db import models
+import address
 
 class Restaurant(models.Model):
     name = models.CharField(max_length=100)
@@ -18,6 +19,16 @@ class Restaurant(models.Model):
         if not hasattr(self, '_current_location'):
             self._current_location = self.updates.latest().location
         return self._current_location
+        
+    @staticmethod
+    def from_json(json):
+        r = Restaurant()
+        
+        r.name = json['name']
+        r.website = json['url']
+        r.twitter_id = json['id']
+        
+        return r
 
 
 class Update(models.Model):
@@ -26,9 +37,32 @@ class Update(models.Model):
     url = models.URLField()
     location = models.PointField(null=True)
     timestamp = models.DateTimeField(default=datetime.datetime.now)
-    twitter_status_id = models.IntegerField("Twitter Status ID", max_length=20, blank=False) # useful for ensuring we get only fresh updates when we query
+    twitter_status_id = models.IntegerField("Twitter Status ID", blank=False) # useful for ensuring we get only fresh updates when we query
 
     objects = models.GeoManager()
+    
+    def __str__(self):
+        return "%s: %s @ %s (%s)" % (self.restaurant.name, self.update, self.timestamp, self.twitter_status_id)
 
     class Meta:
         get_latest_by='timestamp'
+        
+    @staticmethod    
+    def from_json(json):
+        u = Update()
+        u.update = json['text']
+        #u.timestamp = json['created_at']
+        u.twitter_status_id = json['id']
+        
+        try:
+            restaurant = Restaurant.objects.get(twitter_id=json['user']['id'])
+        except Restaurant.DoesNotExist:
+            restaurant = Restaurant.from_json(json['user'])
+
+        u.restaurant = restaurant
+        
+        print u
+        
+        return u
+        
+
