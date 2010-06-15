88ada4b27c311d3284aca05c137ea2ac51610d20
Ethan Phelps-Goodman <ethan@sunlightfoundation.com>
2010-06-15 11:59:33 -0400

    Started work on view of latest tweets.

--- ftruck/views.py
+++ ftruck/views.py
@@ -8,3 +8,14 @@ def mainmap(request):
 
     return render_to_response('mainmap.html',
                               {'restaurants':restaurants})
+
+
+def tweets(request):
+    """ render a list of the latest tweets from today """
+
+    updates = [r.updates.latest() for r in Restaurant.objects.all()]
+    
+    return render_to_response('tweets.html',
+                               {'updates': updates})
+
+
