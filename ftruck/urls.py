0a7f8498eb870f3aa7e80fffd32498926c27c9ae
James Turk <james.p.turk@gmail.com>
2010-07-08 16:07:06 -0400

    rename map view to /

--- ftruck/urls.py
+++ ftruck/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('ftruck.views',
-    url(r'^map/', 'mainmap', name='map'),
-    url(r'^tweets/', 'tweets', name='tweets')
+    url(r'^$', 'mainmap', name='map'),
+    url(r'^tweets/$', 'tweets', name='tweets')
 )
